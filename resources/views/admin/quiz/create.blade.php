4a97befc86dcef79977236b97ff4f4a6cdf5d13c
Batuhan Arslan <bthnarsln64@gmail.com>
2021-02-09 17:12:20 +0300

    form validasyonu yapıldı, laravel tr kütüphanesi kuruldu ve hata mesajları Türkçeye çevrildi, quiz verileri veritabanına kayıt edildi.
    Signed-off-by: Batuhan Arslan <bthnarsln64@gmail.com>

--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -6,19 +6,21 @@
                 @csrf
                 <div class="form-group">
                     <label for="title">Quiz Başlığı</label>
-                    <input type="text" name="title" class="form-control" required>
+                    <input type="text" name="title" class="form-control" value="{{ old('title') }}">
                 </div>
                 <div class="form-group">
                     <label for="description">Quiz Açıklama</label>
-                    <textarea name="description" class="form-control" rows="4"></textarea>
+                    <textarea name="description" class="form-control" rows="4"
+                        value="{{ old('description') }}"></textarea>
                 </div>
                 <div class="form-group">
-                    <input id="isFinished" type="checkbox">
+                    <input id="isFinished" @if (old('finished_at')) checked @endif type="checkbox">
                     <label for="finished_at">Bitiş Tarihi Olacak mı?</label>
                 </div>
-                <div id="finishedInput" style="display: none;" class="form-group">
+                <div id="finishedInput" @if (!old('finished_at')) style="display: none;" @endif class="form-group">
                     <label for="finished_at">Bitiş Tarihi</label>
-                    <input type="datetime-local" name="finished_at" class="form-control">
+                    <input type="datetime-local" name="finished_at" class="form-control"
+                        value="{{ old('finished_at') }}">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,13 +41,30 @@
 
         <div class="py-6">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        <i class="fa fa-check"></i>
+                        {{ session('success') }}
+                    </div>
+                @endif
+
                 {{ $slot }}
             </div>
         </div>
     </div>
 
     @stack('modals')
-    {{ $js }}
+    @isset($js)
+        {{ $js }}
+    @endisset
     @livewireScripts
 </body>
 
